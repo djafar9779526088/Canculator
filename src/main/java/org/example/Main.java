94996f226e4f4b1ee8eee10ce71fb0585799c9d3
elik <d79779526088@gmail.com>
2025-09-15 18:03:22 +0300

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("sdsdf");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите 1 число: ");
+        int a = scan.nextInt();
+
+        int result;
+
+        System.out.println("-" + "+" + "*" + "/");
+        char operator = scan.next().charAt(0);
+
+        System.out.println("Введите 2 число");
+        int b = scan.nextInt();
+
+        System.out.println("Ответ: ");
+        switch (operator) {
+            case '-':
+                System.out.println(a - b);
+                break;
+
+            case '+':
+                System.out.println(a + b);
+                break;
+
+            case '*':
+                System.out.println(a * b);
+                break;
+
+            case '/':
+                if(b == 0) {
+                    System.out.println("делить на ноль нельзя");
+                }else {
+                    result = a / b;
+                    System.out.println(a / b);
+                    break;
+                }
+                break;
+            default:
+                System.out.println("error");
+                break;
+        }
     }
 }
